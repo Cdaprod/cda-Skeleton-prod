324dee6ce3024a723fa1f66550e2a1c0b3c888a6
root <root@cda-LAPTOP>
2023-04-25 19:28:21 -0400

    init vscode

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,54 @@
+<script>
+		// The ordering of these imports is critical to your app working properly
+
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+		// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+
+	import '@skeletonlabs/skeleton/styles/all.css';
+	
+		// Most of your app wide CSS should be put in this file
+	
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase sticky top right">David Cannan</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/cdasmktcda"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/Cdaprod"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://linkedin.com/in/cdasmkt"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Linkedin
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/SectionCard.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let header = "";
+    export let content = "";
+    export let image = "";
+    export let link = "";
+    export let altColor = false;
+  </script>
+  
+  <div class={`section-card ${altColor ? 'alt-color' : ''}`}>
+    <h3>{header}</h3>
+    <p>{content}</p>
+    <img src={image} alt={header} />
+    <a href={link} target="_blank">Learn more</a>
+  </div>
+  
+  <style>
+    .section-card {
+      /* Your default styling for the section card */
+ 
+      /* Additional styles */
+    }
+  
+    .section-card.alt-color {
+      /* Your alternative color styling for the section card */
+
+      /* Additional styles */
+    }
+  </style>
+  
