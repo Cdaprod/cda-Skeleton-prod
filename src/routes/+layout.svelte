7c0d7160193c0ddb93964fe86ed888acb6d37273
David <104325852+Cdaprod@users.noreply.github.com>
2023-04-27 22:38:36 -0400

    Update Avatar src path in +layout.svelte file.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -43,7 +43,7 @@
 				>
 					Linkedin
 				</a>
-				<Avatar src="https://github.com/cdaprod/cda.public/avatar.jpg" style="filter: url({'#Noir'})" border="border-4 	border-surface-300-600-token hover:!border-primary-500"
+				<Avatar src="../../../../static/avatar.jpg" style="filter: url({'#Noir'})" border="border-4 	border-surface-300-600-token hover:!border-primary-500"
 	cursor="cursor-pointer" />
 			</svelte:fragment>
 		</AppBar>
