15ff70ba528d04c872d6b825cd707efa7307e664
David <104325852+Cdaprod@users.noreply.github.com>
2023-04-28 00:47:09 -0400

    Refactor page layout
    Removed unnecessary container div and adjusted grid layout to be more re
    sponsive.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,13 +35,11 @@
 		</div>
 	</div>
 </div>
-<div class="container min-h-screen mx-30 flex flex-col md:flex-row md:grid md:grid-cols-3 gap-x-3 gap-y-6 justify-center items-center overflow-y-auto pl-5 md:px-auto">
-auto">
+<div class="bg-blue-500 bg-opacity-20 rounded-lg shadow-md container min-h-screen mx-30 flex flex-col md:flex-row md:grid md:grid-cols-3 gap-x-3 gap-y-6 justify-center items-center overflow-y-auto pl-5 md:px-auto">
 {#if pageContent && pageContent.sections}
  {#each pageContent.sections as section, index}
   <Accordion>
 	<AccordionItem>
-	
 		<svelte:fragment slot="summary">{section.header}</svelte:fragment>
 		<svelte:fragment slot="content">{section.content}</svelte:fragment>
 	</AccordionItem>
