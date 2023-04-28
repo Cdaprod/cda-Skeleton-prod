aac203c68cef2c2198bec2ce9a176f5115e80e2c
David <104325852+Cdaprod@users.noreply.github.com>
2023-04-28 00:30:37 -0400

    Remove image and link from SectionCard component.

--- src/routes/SectionCard.svelte
+++ src/routes/SectionCard.svelte
@@ -9,8 +9,6 @@
 <div class={`section-card ${altColor ? 'alt-color' : ''} p-4 bg-blue-500 bg-opacity-20 rounded-lg shadow-md`}>
   <h3 class="text-xl font-semibold mb-2">{header}</h3>
   <p class="text-white-700 mb-4">{content}</p>
-  <img src={image} alt={header} class="w-full h-auto mb-4" />
-  <a href={link} target="_blank" class="text-blue-600 hover:text-blue-800">Learn more</a>
 </div>
 
 <style lang="postcss">
